e51ddac3857419603e8d87d66232288647d1dde2
chinjon <Jonathankchin@gmail.com>
2016-12-17 16:35:08 -0500

    added favicon not sure if will work || started work on front-end and functionality

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+// API REPO:  https://github.com/Giphy/GiphyAPI
+
+// public key = dc6zaTOxFJmzC
+
+//
+
+window.onload = function() {
+
+  var searchTerms = ["cats", "basketball"];
+
+
+
+
+}
+
+
+
+
+
+$("#search").on("click", function(e){
+
+  e.preventDefault();
+
+  var userQuery = $('#inputBox').val();
+  console.log(userQuery);
+
+  var key = "&api_key=dc6zaTOxFJmzC";
+  var limit = "&limit=5"
+  var reqUrl = "http://api.giphy.com/v1/gifs/search?q="+userQuery+limit+key;
+  console.log(reqUrl);
+  $.ajax({
+    url: reqUrl,
+    method: "GET"
+  }).done(function(response){
+
+    console.log(response.data[0].images["fixed_height"].url);
+    var imgLink = response.data[0].images["fixed_height"].url;
+    var newImg = $('<img>');
+
+    newImg.attr('src', imgLink);
+    $('#showGIFS').append(newImg);
+
+  })
+
+
+})
